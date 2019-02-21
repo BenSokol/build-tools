62b76504d4f31dbdb6c77e268e515a25ced0654c
Ben Sokol <ben@bensokol.com>
2019-02-21 04:22:10 -0600

    Change cppcheck to c++11 for compatibility

--- make-utilities/cppcheck.mk
+++ make-utilities/cppcheck.mk
@@ -2,7 +2,7 @@
 # @Author:   Ben Sokol <Ben>
 # @Email:    ben@bensokol.com
 # @Created:  February 14th, 2019 [5:02pm]
-# @Modified: February 19th, 2019 [12:51am]
+# @Modified: February 21st, 2019 [4:21am]
 # @Version:  1.0.0
 #
 # Copyright (C) 2019 by Ben Sokol. All Rights Reserved.
@@ -10,5 +10,5 @@
 ifndef MAKEFILE_RULE_CPPCHECK_DISABLE
 .PHONY: cppcheck
 cppcheck: $(SOURCES)
-	@printf "Running cppcheck...\n" && cppcheck -q --suppress=preprocessorErrorDirective:submodules/utilities/UTL_textWrap.cpp:17 -D__cplusplus --std=c11 --std=c++14 --force --error-exitcode=1 --enable=warning $(SRCDIR)/* $(SUBDIR)/* -I $(SRCDIR)/* -I $(SUBDIR)/* || true
+	@printf "Running cppcheck...\n" && cppcheck -q --suppress=preprocessorErrorDirective:submodules/utilities/UTL_textWrap.cpp:17 -D__cplusplus --std=c11 --std=c++11 --force --error-exitcode=1 --enable=warning $(SRCDIR)/* $(SUBDIR)/* -I $(SRCDIR)/* -I $(SUBDIR)/* || true
 endif
