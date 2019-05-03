0d21c341881791e0174203eee43540968244a3ae
Ben Sokol <ben@bensokol.com>
2019-05-03 18:25:59 -0500

    Add LDFLAGS and CUSTOMBUILD

--- make-rules/wildcard/c.mk
+++ make-rules/wildcard/c.mk
@@ -2,11 +2,12 @@
 # @Author:   Ben Sokol <Ben>
 # @Email:    ben@bensokol.com
 # @Created:  February 14th, 2019 [5:06pm]
-# @Modified: February 15th, 2019 [12:49pm]
+# @Modified: May 3rd, 2019 [7:01pm]
 # @Version:  1.0.0
 #
 # Copyright (C) 2019 by Ben Sokol. All Rights Reserved.
 
+ifndef CUSTOMBUILD
 $(OBJDIR)/%.o: %.c %.h .pre_build
 	@printf "[%02d/%02d] Compiling C object $(subst $(OBJDIR)/,,$(@))\n" `expr $(CURRENT_STEP)` $(TOTAL_STEPS) #$(ECHOCOUNT)
 	@$(CC) $(CFLAGS) -c -MT $@ -MD -MP -MF $(DEPDIR)/$(patsubst %.o,%,$(subst $(OBJDIR)/,,$(@))).Td $< -o $@
@@ -16,3 +17,4 @@ $(OBJDIR)/%.o: %.c .pre_build
 	@printf "[%02d/%02d] Compiling C object $(subst $(OBJDIR)/,,$(@))\n" `expr $(CURRENT_STEP)` $(TOTAL_STEPS) #$(ECHOCOUNT)
 	@$(CC) $(CFLAGS) -c -MT $@ -MD -MP -MF $(DEPDIR)/$(patsubst %.o,%,$(subst $(OBJDIR)/,,$(@))).Td $< -o $@
 	@mv -f $(DEPDIR)/$(patsubst %.o,%,$(subst $(OBJDIR)/,,$(@))).Td $(DEPDIR)/$(patsubst %.o,%,$(subst $(OBJDIR)/,,$(@))).d
+endif
--- make-rules/wildcard/cpp.mk
+++ make-rules/wildcard/cpp.mk
@@ -2,12 +2,12 @@
 # @Author:   Ben Sokol <Ben>
 # @Email:    ben@bensokol.com
 # @Created:  February 14th, 2019 [5:06pm]
-# @Modified: February 15th, 2019 [12:49pm]
+# @Modified: May 3rd, 2019 [7:01pm]
 # @Version:  1.0.0
 #
 # Copyright (C) 2019 by Ben Sokol. All Rights Reserved.
 
-
+ifndef CUSTOMBUILD
 $(OBJDIR)/%.o: %.cpp %.hpp .pre_build
 	@printf "[%02d/%02d] Compiling CXX object $(subst $(OBJDIR)/,,$(@))\n" `expr $(CURRENT_STEP)` $(TOTAL_STEPS) #$(ECHOCOUNT)
 	@$(CXX) $(CXXFLAGS) -c -MT $@ -MD -MP -MF $(DEPDIR)/$(patsubst %.o,%,$(subst $(OBJDIR)/,,$(@))).Td $< -o $@
@@ -17,3 +17,4 @@ $(OBJDIR)/%.o: %.cpp .pre_build
 	@printf "[%02d/%02d] Compiling CXX object $(subst $(OBJDIR)/,,$(@))\n" `expr $(CURRENT_STEP)` $(TOTAL_STEPS) #$(ECHOCOUNT)
 	@$(CXX) $(CXXFLAGS) -c -MT $@ -MD -MP -MF $(DEPDIR)/$(patsubst %.o,%,$(subst $(OBJDIR)/,,$(@))).Td $< -o $@
 	@mv -f $(DEPDIR)/$(patsubst %.o,%,$(subst $(OBJDIR)/,,$(@))).Td $(DEPDIR)/$(patsubst %.o,%,$(subst $(OBJDIR)/,,$(@))).d
+endif
