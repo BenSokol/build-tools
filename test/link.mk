9c16e56547fb214bfb807c6395db507268439628
Ben Sokol <ben@bensokol.com>
2019-10-20 04:15:27 -0500

    Fix output when building test

--- test/link.mk
+++ test/link.mk
@@ -2,7 +2,7 @@
 # @Author:   Ben Sokol <Ben>
 # @Email:    ben@bensokol.com
 # @Created:  February 14th, 2019 [5:08pm]
-# @Modified: October 20th, 2019 [12:43am]
+# @Modified: October 20th, 2019 [4:15am]
 # @Version:  1.0.0
 #
 # Copyright (C) 2019 by Ben Sokol. All Rights Reserved.
@@ -10,7 +10,7 @@
 ifndef MAKEFILE_RULE_TEST_DISABLE
 ifndef CUSTOMBUILD
 $(EXE)-test: .pre_build $(strip $(TESTOBJECTS)) $(LIB)
-	@printf "[%02d/%02d] Linking $(EXE)\n" `expr $(CURRENT_STEP)` $(TOTAL_STEPS) #$(ECHOCOUNT)
+	@printf "[%02d/%02d] Linking $(EXE)-test\n" `expr $(CURRENT_STEP)` $(TOTAL_STEPS) #$(ECHOCOUNT)
 	@$(CXX) $(TESTLDFLAGS) -o $(EXE)-test $(TESTOBJECTS) $(LIB)
 endif
 endif
