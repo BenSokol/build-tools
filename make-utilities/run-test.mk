c25b2a8ebdf49a1644520e2366d1005d01c870b7
Ben Sokol <ben@bensokol.com>
2019-10-20 19:36:56 -0500

    Update run-test

--- make-utilities/run-test.mk
+++ make-utilities/run-test.mk
@@ -2,19 +2,28 @@
 # @Author:   Ben Sokol <Ben>
 # @Email:    ben@bensokol.com
 # @Created:  February 14th, 2019 [5:02pm]
-# @Modified: October 20th, 2019 [3:40am]
+# @Modified: October 20th, 2019 [7:37pm]
 # @Version:  1.0.0
 #
 # Copyright (C) 2019 by Ben Sokol. All Rights Reserved.
 
 ifndef MAKEFILE_RULE_RUN_DISABLE
 ifndef MAKEFILE_RULE_TEST_DISABLE
-.PHONY: run-test
+
+.PHONY: run-test run-tests
+ifdef MAKEFILE_RULE_CPPCHECK_DISABLE
 run-test: test
 	@printf "Running mod tests...\n"
 	@./$(TESTEXE) --reporter compact --success
-ifndef MAKEFILE_RULE_CPPCHECK_DISABLE
-	@make cppcheck
 endif
+
+ifndef MAKEFILE_RULE_CPPCHECK_DISABLE
+run-test: test cppcheck
+	@printf "Running mod tests...\n"
+	@./$(TESTEXE) --reporter compact --success
+endif
+
+run-tests: run-test
+
 endif
 endif
